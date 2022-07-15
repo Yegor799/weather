700d6e41389371c90a39b698e87c67cbff453265
Yegor <you@example.com>
2022-07-15 14:54:48 +0300

    -

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '9f9b7b8359msh1739a092a463ca7p102f3ejsnbeccf5c14b2e',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
+
